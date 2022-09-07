a904c6481bad6b6d0dd85305e0c6fbe3585201f8
greffnoah <greffnoah@gmail.com>
2022-09-07 20:11:21 +0200

    added `COM_PORT`

--- src/main/java/me/noahgreff/s4ct/Main.java
+++ src/main/java/me/noahgreff/s4ct/Main.java
@@ -8,42 +8,19 @@ import java.util.Scanner;
 @SuppressWarnings("unused")
 public class Main extends JavaPlugin {
 
+    public static final String COM_PORT = "COM3";
+
     @Override
     public void onLoad() {}
 
     @Override
     public void onEnable() {
-//        SerialPort[] ports = SerialPort.getCommPorts();
-//
-//        System.out.println("Select a port:");
-//        for (int i = 0; i < ports.length; i++) {
-//            System.out.println(i +  ": " + ports[i].getSystemPortName());
-//        }
-//
-//        Scanner scanner = new Scanner(System.in);
-//        SerialPort serialPort = ports[scanner.nextInt()];
-//
-//        if (serialPort.openPort()) {
-//            System.out.println("Port opened successfully.");
-//        } else {
-//            System.out.println("Unable to open the port.");
-//            return;
-//        }
-//
-//        //serialPort.setComPortParameters(9600, 8, 1, SerialPort.NO_PARITY);
-//        serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
-//        Scanner data = new Scanner(ports[0].getInputStream());
-//
-//        while (data.hasNextLine()) {
-//            System.out.println(data.nextLine());
-//        }
-        SerialPort port = SerialPort.getCommPort("COM3");
+        SerialPort port = SerialPort.getCommPort(COM_PORT);
 
         if (!port.openPort()) {
-            throw new RuntimeException("Could not open serial port 'COM3'.");
+            throw new RuntimeException("Could not open serial port '" + COM_PORT + "'.");
         }
 
-        //serialPort.setComPortParameters(9600, 8, 1, SerialPort.NO_PARITY);
         port.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
         Scanner data = new Scanner(port.getInputStream());
 
