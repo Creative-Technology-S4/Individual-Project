103438e916916d9889bb41a695c04a2ddd79a345
greffnoah <greffnoah@gmail.com>
2022-09-08 16:11:25 +0200

    fixed threading issue between stream thread and game thread

--- src/main/java/me/noahgreff/s4ct/Main.java
+++ src/main/java/me/noahgreff/s4ct/Main.java
@@ -6,6 +6,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicReference;
 
 @SuppressWarnings({"unused", "StatementWithEmptyBody"})
 public class Main extends JavaPlugin {
@@ -13,38 +14,41 @@ public class Main extends JavaPlugin {
     private static final String COM_PORT = "COM3";
     private static final int BAUD_RATE = 9600;
 
-    private static Thread THREAD;
+    private static final AtomicReference<Integer> VALUE = new AtomicReference<>(0);
+
+    private static final Thread SYNCHRONIZER = new Thread(() -> {
+        SerialPort port = SerialPort.getCommPort(COM_PORT);
+        port.setComPortParameters(BAUD_RATE, 8, 1, 0);
+        port.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
+
+        while (!port.openPort()) { } // continuously attempt to open port
+        Scanner stream = new Scanner(port.getInputStream());
+
+        while (stream.hasNextLine()) {
+            try {
+                VALUE.set(stream.nextInt());
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        }
+    });
 
     @Override
     public void onEnable() {
-        THREAD = new Thread(() -> {
-            SerialPort port = SerialPort.getCommPort(COM_PORT);
-            port.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 0, 0);
-            port.setBaudRate(BAUD_RATE);
+        SYNCHRONIZER.start();
 
-            while (!port.openPort()) { } // continuously attempt to open port
-
-            Scanner stream = new Scanner(port.getInputStream());
-
-            while (stream.hasNextLine()) {
-                try {
-                    int value = Integer.parseInt(stream.nextLine());
-                    long time = (long) Math.clampedMap(value, 0, 1023, 0, 24000);
-                    Bukkit.getWorld("world").setTime(time);
-                    System.out.println(time);
-                } catch (Exception exception) {
-                    exception.printStackTrace();
-                }
+        getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
+            try {
+                long time = (long) Math.clampedMap(VALUE.get(), 0, 1023, 0, 24000);
+                Bukkit.getWorld("world").setTime(time);
+            } catch (Exception exception) {
+                exception.printStackTrace();
             }
-
-            throw new RuntimeException("Stream has abruptly stopped.");
-        });
-
-        THREAD.start();
+        }, 0, 20);
     }
 
     @Override
     public void onDisable() {
-        THREAD.interrupt();
+        SYNCHRONIZER.interrupt();
     }
 }
